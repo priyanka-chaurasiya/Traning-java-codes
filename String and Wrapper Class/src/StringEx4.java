d63728b6d3b66f59f8842c53ca3da5773ee70083
priyanka chaurasiya <priyanka chaurasiya@Priyanka>
2026-07-07 17:43:45 +0530

    string and wrapper class

---
+++ String and Wrapper Class/src/StringEx4.java
@@ -0,0 +1,10 @@
+public class StringEx4 {
+	public static void main(String[] args) {
+		String s1=new String("Priyanka");
+		String s2=new String("Yoyo");
+		String s3="Priyanka";
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+	}
+}
